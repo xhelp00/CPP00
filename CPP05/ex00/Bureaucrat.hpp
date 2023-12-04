bf546a87702baffac83ecd66f6fd7a7144e4aa75
Petr Helebrant <xhelp00@gmail.com>
2023-12-04 14:18:44 +0100

    ex00

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -2,17 +2,43 @@
 #define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <string>
+#include <stdexcept>
+
+#define MAX_GRADE 1
+#define MIN_GRADE 150
 
 class Bureaucrat
 {
 	private:
+			std::string	const 	_name;
+			int					_grade;
 
 	public:
 			Bureaucrat();
-			Bureaucrat(const Bureaucrat &other);
+			Bureaucrat(const std::string& name, int grade);
+			Bureaucrat(const Bureaucrat& other);
 			Bureaucrat& operator=(const Bureaucrat &other);
 			~Bureaucrat();
+
+			const std::string& getName() const;
+			int			getGrade() const;
+
+			void	gradeUp(int up);
+			void	gradeDown(int down);
+
+			class GradeTooLowException: public std::exception 
+			{
+			virtual const char*	what() const throw();
+			};
+
+			class GradeTooHighException: public std::exception
+			{
+			virtual const char*	what() const throw();
+			};
+
 };
 
+std::ostream&	operator<<(std::ostream &o, const Bureaucrat& bureaucrat);
 
 #endif
--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -1,7 +1,69 @@
 #include "Bureaucrat.hpp"
 
-int main()
+void	sectionTitle(const std::string& title)
 {
-	
-	return 0;
+	std::cout	<< std::endl
+				<< "*** " << title << " ***"
+				<< std::endl << std::endl;
+}
+
+int	main() {
+	sectionTitle("Constructor test");
+
+	try {
+		Bureaucrat	minister("minister", 1);
+		std::cout << minister;
+	}
+	catch (std::exception& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	try {
+		Bureaucrat	test("test", -42);
+		std::cout << test;
+	}
+	catch (std::exception& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+
+	try {
+		Bureaucrat	test("test", 4242);
+		std::cout << test;
+	}
+	catch (std::exception& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	sectionTitle("Increment test");
+
+	try {
+		Bureaucrat	minister("minister", 12);
+		std::cout << minister;
+		minister.gradeUp(10);
+		std::cout << minister;
+		minister.gradeUp(1);
+		std::cout << minister;
+		minister.gradeUp(1); // too high!
+		std::cout << minister;
+	}
+	catch (std::exception& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	sectionTitle("Decrement test");
+
+	try {
+		Bureaucrat	minister("minister", 139);
+		std::cout << minister;
+		minister.gradeDown(10);
+		std::cout << minister;
+		minister.gradeDown(1);
+		std::cout << minister;
+		minister.gradeDown(1); // too low!
+		std::cout << minister;
+	}
+	catch (std::exception& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
 }
